b9f5e49c4bd6aba05fb4991941a92f1bc10f4e16
Movchan Alexsander <seosasha@MacBook-Air-Movchan.local>
2025-10-26 22:50:40 +0200

    fix: named export supabase from lib/supabaseClient

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,6 @@
-'use client';
-
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseBrowser = () => {
-  const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-  return createClient(url, anon);
-};
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+);
