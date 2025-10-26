91271427a1af2ec0c401095518a0979b53d549c1
sigmetapp <admin@sigmet.app>
2025-10-27 01:12:31 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
+import Layout from "@/components/Layout";
 import { SiteSettingsProvider } from "@/components/SiteSettingsContext";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SiteSettingsProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </SiteSettingsProvider>
   );
 }
